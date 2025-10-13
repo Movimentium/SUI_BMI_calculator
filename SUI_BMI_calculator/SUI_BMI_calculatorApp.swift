d62a5a889db8073a03bd462f5d15a819cb0b80d0
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-10-14 00:21:19 +0200

    Initial Commit

---
+++ SUI_BMI_calculator/SUI_BMI_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUI_BMI_calculatorApp.swift
+//  SUI_BMI_calculator
+//
+//  Created by Miguel Gallego on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SUI_BMI_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
