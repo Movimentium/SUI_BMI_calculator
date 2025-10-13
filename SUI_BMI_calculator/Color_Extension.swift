b9d8642afa0390bb6541c3d8771c4d1039b30e53
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-10-14 00:59:02 +0200

    WIP MyCardView

---
+++ SUI_BMI_calculator/Color_Extension.swift
@@ -0,0 +1,51 @@
+//  Color_Extension.swift
+//  SUI_BMI_calculator
+import UIKit
+import SwiftUI
+
+
+extension Color {
+    
+    struct card {
+        static let bg = Color(red: 0.97, green: 0.95, blue: 0.98)
+        static let blackShadow = Color.black.opacity(0.25)
+    }
+}
+
+// MARK: - Legacy
+extension UIColor {
+    
+    //  Usage:
+    //  let color = UIColor(red: 0xFF, green: 0xFF, blue: 0xFF)
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0   && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0  && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0,
+                  green: CGFloat(green) / 255.0,
+                  blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    //  Usage:
+    //  let color2 = UIColor(rgb: 0xFFFFFF)
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+    
+    // Get a image from color. Usage:
+    // let image0 = UIColor.orange.image(CGSize(width: 128, height: 128))
+    // let image1 = UIColor.yellow.image()
+    func image(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { rendererContext in
+            self.setFill()
+            rendererContext.fill(CGRect(origin: .zero, size: size))
+        }
+    }
+  
+}
+
--- SUI_BMI_calculator/ContentView.swift
+++ SUI_BMI_calculator/ContentView.swift
@@ -5,16 +5,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 18) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Spacer()
+            
+            MyCardView()
+            
+            Spacer()
         }
+//        .frame(minWidth: .infinity)
+        .border(.red)
         .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+/*
+ 
+ */
+
+struct MyCardView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 0) {
+            Text("Placeholder")
+                .padding(20)
+        }
+        .padding(.vertical)
+        .background(Color.card.bg)
+        .clipShape(RoundedRectangle(cornerRadius: 18))
+        .shadow(color: .card.blackShadow, radius: 5, x: 0, y: 2)
+    }
 }
